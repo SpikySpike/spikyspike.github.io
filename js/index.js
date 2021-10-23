667f705129afb93f85cdf72ac2466fd1e2dc0c24
SpikySpike <87228343+SpikySpike@users.noreply.github.com>
2021-10-23 19:56:11 +0300

    committed changes 😱

---
+++ js/index.js
@@ -0,0 +1,5 @@
+console.log(
+    "%cWhat are you doing here? 🤨\n%cThere's nothing here, the website is the simplest,\ndon't waste your time looking at this!", 
+    "font-size: 400%; color: #7209b7; font-weight: bold;",
+    "font-size: 200%; color: #5fe863; font-weight: normal;"
+)
